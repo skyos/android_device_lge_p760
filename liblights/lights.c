93281b38437bc3244eb580deca40778fc6fb4faf
mateo1111 <mateo1111@gmail.com>
2013-08-28 12:24:45 +0200

    Revert: "Update LgeL9Parts" Reverts this commit: https://github.com/mateo1111/android_device_lge_p760/commit/c8a6c1e31cb2fc4058a01e93eeca66689cd1cb42

--- liblights/lights.c
+++ liblights/lights.c
@@ -37,14 +37,10 @@ static pthread_once_t g_init = PTHREAD_ONCE_INIT;
 static pthread_mutex_t g_lock = PTHREAD_MUTEX_INITIALIZER;
 
 char const*const LCD_FILE
-        = "/sys/devices/platform/omap/omap_i2c.2/i2c-2/2-0038/brightness";
-
+        = "/sys/devices/platform/omap/omap_i2c.2/i2c-2/2-0036/brightness";
 char const*const KEYBOARD_FILE
         = "/sys/class/leds/button-backlight/brightness";
 
-// sysfs file for BLN
-char const*const NOTIFICATION_FILE
-        = "/sys/class/misc/backlightnotification/notification_led";
 
 void init_globals(void)
 {
@@ -117,18 +113,6 @@ set_light_buttons(struct light_device_t* dev,
 }
 
 static int
-set_light_notifications(struct light_device_t* dev,
-        struct light_state_t const* state)
-{
-    pthread_mutex_lock(&g_lock);
-    int on = is_lit(state);
-    int err = write_int(NOTIFICATION_FILE, on ? 1:0); // BLN
-    pthread_mutex_unlock(&g_lock);
-    return 0;
-}
-
-
-static int
 close_lights(struct light_device_t *dev)
 {
     if (dev) {
@@ -147,9 +131,6 @@ static int open_lights(const struct hw_module_t* module, char const* name,
 
     if (0 == strcmp(LIGHT_ID_BACKLIGHT, name)) {
         set_light = set_light_backlight;
-    }
-    else if (0 == strcmp(LIGHT_ID_NOTIFICATIONS, name)) {
-        set_light = set_light_notifications;
     }
     else if (0 == strcmp(LIGHT_ID_BUTTONS, name)) {
         set_light = set_light_buttons;
